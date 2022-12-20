8b82bbe862da1adabb1d19997cfcc1b3c12ec248
HaPBoy <ihapboy@gmail.com>
2022-12-20 18:19:52 +0800

    feat: add helpers (transformer)

---
+++ packages/heplers/src/transformer.ts
@@ -0,0 +1,108 @@
+import { Extension, getSchema, JSONContent } from '@tiptap/core';
+import {
+  Node as ProseMirrorNode,
+  Schema,
+  DOMParser,
+  DOMSerializer,
+  ParseOptions,
+} from 'prosemirror-model';
+import { parseHTML, createHTMLDocument, VHTMLDocument } from 'zeed-dom';
+import type { MarkdownEditor } from '@test-pkgs/markdown';
+
+export class Transformer {
+  schema: Schema;
+
+  parser: DOMParser;
+
+  serializer: DOMSerializer;
+
+  constructor(schema: Schema) {
+    this.schema = schema;
+    this.parser = DOMParser.fromSchema(schema);
+    this.serializer = DOMSerializer.fromSchema(schema);
+  }
+
+  // HTML to ProseMirror Node
+  htmlToNode(html: string, options?: ParseOptions): ProseMirrorNode {
+    const dom = parseHTML(html) as unknown as Node;
+    return this.parser.parse(dom, options);
+  }
+
+  // ProseMirror Node to HTML
+  nodeToHTML(node: ProseMirrorNode): string {
+    const document = this.serializer.serializeFragment(node.content, {
+      document: createHTMLDocument() as unknown as Document,
+    }) as unknown as VHTMLDocument;
+
+    return document.render();
+  }
+
+  // HTML to JSONContent
+  htmlToJSON(html: string): JSONContent {
+    return this.htmlToNode(html).toJSON();
+  }
+
+  // JSONContent to HTML
+  jsonToHTML(doc: JSONContent): string {
+    const contentNode = ProseMirrorNode.fromJSON(this.schema, doc);
+    return this.nodeToHTML(contentNode);
+  }
+
+  // ProseMirror Node to JSONContent
+  nodeToJSON(node: ProseMirrorNode): JSONContent {
+    return node.toJSON();
+  }
+
+  // JSONContent to ProseMirror Node
+  jsonToNode(doc: JSONContent): ProseMirrorNode {
+    return ProseMirrorNode.fromJSON(this.schema, doc);
+  }
+
+  // Markdown to HTML
+  markdownToHTML(markdown: string, editor: MarkdownEditor): string {
+    return editor.parseMarkdown(markdown);
+  }
+
+  // Markdown to ProseMirror Node
+  markdownToNode(markdown: string, editor: MarkdownEditor): ProseMirrorNode {
+    return this.htmlToNode(this.markdownToHTML(markdown, editor));
+  }
+
+  // Markdown to JSONContent
+  markdownToJSON(markdown: string, editor: MarkdownEditor): JSONContent {
+    return this.markdownToNode(markdown, editor).toJSON();
+  }
+
+  // ProseMirror Node to Markdown
+  nodeToMarkdown(node: ProseMirrorNode, editor: MarkdownEditor): string {
+    return editor.getMarkdown(node);
+  }
+
+  // JSONContent to Markdown
+  jsonToMarkdown(doc: JSONContent, editor: MarkdownEditor): string {
+    return this.nodeToMarkdown(this.jsonToNode(doc), editor);
+  }
+
+  // HTML to Markdown
+  htmlToMarkdown(html: string, editor: MarkdownEditor): string {
+    return this.nodeToMarkdown(this.htmlToNode(html), editor);
+  }
+
+  // Markdown to Markdown
+  markdownToMarkdown(markdown: string, editor: MarkdownEditor): string {
+    return this.nodeToMarkdown(this.markdownToNode(markdown, editor), editor);
+  }
+
+  // HTML to HTML
+  htmlToHTML(html: string): string {
+    return this.nodeToHTML(this.htmlToNode(html));
+  }
+
+  static fromSchema(schema: Schema) {
+    return new Transformer(schema);
+  }
+
+  static fromExtensions(extensions: Extension[]) {
+    return new Transformer(getSchema(extensions));
+  }
+}
