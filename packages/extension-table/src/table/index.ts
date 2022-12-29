73997169ab5f563c72df117e6c4de30da3d9b594
HaPBoy <ihapboy@gmail.com>
2022-12-29 12:25:16 +0800

    fix: table column resizing handle

--- packages/extension-table/src/table/index.ts
+++ packages/extension-table/src/table/index.ts
@@ -2,5 +2,26 @@ import {
   Table as TTable,
   TableOptions as TTableOptions,
 } from '@tiptap/extension-table';
+import { tableEditing } from '@tiptap/prosemirror-tables';
+import { columnResizing } from './columnresizing';
 
-export const Table = TTable.extend<TTableOptions>({});
+export const Table = TTable.extend<TTableOptions>({
+  addProseMirrorPlugins() {
+    const isResizable = this.options.resizable && this.editor.isEditable;
+    return [
+      ...(isResizable
+        ? [
+            columnResizing({
+              handleWidth: this.options.handleWidth,
+              cellMinWidth: this.options.cellMinWidth,
+              View: this.options.View,
+              lastColumnResizable: this.options.lastColumnResizable,
+            }),
+          ]
+        : []),
+      tableEditing({
+        allowTableNodeSelection: this.options.allowTableNodeSelection,
+      }),
+    ];
+  },
+});
