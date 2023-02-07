ad8df3f79f495663509592db692acb94cd6d060e
HaPBoy <ihapboy@gmail.com>
2023-02-07 13:46:43 +0800

    fix: 修复任务列表项快捷键 (回车/缩进)

--- packages/extension-task-item/src/task-item.ts
+++ packages/extension-task-item/src/task-item.ts
@@ -91,6 +91,22 @@ export const TaskItem = Node.create<TaskItemOptions>({
     ];
   },
 
+  addKeyboardShortcuts() {
+    const shortcuts = {
+      Enter: () => this.editor.commands.splitListItem(this.name),
+      'Shift-Tab': () => this.editor.commands.liftListItem(this.name),
+    };
+
+    if (!this.options.nested) {
+      return shortcuts;
+    }
+
+    return {
+      ...shortcuts,
+      Tab: () => this.editor.commands.sinkListItem(this.name),
+    };
+  },
+
   addNodeView() {
     return ({ node, HTMLAttributes, getPos, editor }) => {
       const listItem = document.createElement('li');
