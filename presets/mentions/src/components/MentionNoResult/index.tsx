dd11a048e8b3e65565261e9c14cfd236c46b9e76
neil <msg@nancode.cn>
2023-03-31 19:27:06 +0800

    feat: 补充 legacy 样式

--- presets/mentions/src/components/MentionNoResult/index.tsx
+++ presets/mentions/src/components/MentionNoResult/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './index.less';
 
 // todo 国际化
 const NoResult = ({ text = '没有数据' }: { text?: string }) => {
-  return <div className="mention-no-result">{text}</div>;
+  return <div className="gwe-mention-no-result">{text}</div>;
 };
 
 export default NoResult;
