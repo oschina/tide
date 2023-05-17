29306982b08fa95e4a8e0ef6ef617e67993e9245
HaPBoy <ihapboy@gmail.com>
2023-05-17 19:02:37 +0800

    chore: upgrade tiptap to 2.0.3

--- packages/react/src/ReactRenderer.tsx
+++ packages/react/src/ReactRenderer.tsx
@@ -1,95 +1,108 @@
-import { Editor } from '@tiptap/core'
-import React from 'react'
+import { Editor } from '@tiptap/core';
+import React from 'react';
 
-import { Editor as ExtendedEditor } from './Editor'
+import { Editor as ExtendedEditor } from './Editor';
 
 function isClassComponent(Component: any) {
   return !!(
-    typeof Component === 'function'
-    && Component.prototype
-    && Component.prototype.isReactComponent
-  )
+    typeof Component === 'function' &&
+    Component.prototype &&
+    Component.prototype.isReactComponent
+  );
 }
 
 function isForwardRefComponent(Component: any) {
   return !!(
-    typeof Component === 'object'
-    && Component.$$typeof?.toString() === 'Symbol(react.forward_ref)'
-  )
+    typeof Component === 'object' &&
+    Component.$$typeof?.toString() === 'Symbol(react.forward_ref)'
+  );
 }
 
 export interface ReactRendererOptions {
-  editor: Editor,
-  props?: Record<string, any>,
-  as?: string,
-  className?: string,
+  editor: Editor;
+  props?: Record<string, any>;
+  as?: string;
+  className?: string;
+  attrs?: Record<string, string>;
 }
 
 type ComponentType<R, P> =
-  React.ComponentClass<P> |
-  React.FunctionComponent<P> |
-  React.ForwardRefExoticComponent<React.PropsWithoutRef<P> & React.RefAttributes<R>>;
+  | React.ComponentClass<P>
+  | React.FunctionComponent<P>
+  | React.ForwardRefExoticComponent<
+      React.PropsWithoutRef<P> & React.RefAttributes<R>
+    >;
 
 export class ReactRenderer<R = unknown, P = unknown> {
-  id: string
+  id: string;
 
-  editor: ExtendedEditor
+  editor: ExtendedEditor;
 
-  component: any
+  component: any;
 
-  element: Element
+  element: Element;
 
-  props: Record<string, any>
+  props: Record<string, any>;
 
-  reactElement: React.ReactNode
+  reactElement: React.ReactNode;
 
-  ref: R | null = null
+  ref: R | null = null;
 
-  constructor(component: ComponentType<R, P>, {
-    editor,
-    props = {},
-    as = 'div',
-    className = '',
-  }: ReactRendererOptions) {
-    this.id = Math.floor(Math.random() * 0xFFFFFFFF).toString()
-    this.component = component
-    this.editor = editor as ExtendedEditor
-    this.props = props
-    this.element = document.createElement(as)
-    this.element.classList.add('react-renderer')
+  constructor(
+    component: ComponentType<R, P>,
+    {
+      editor,
+      props = {},
+      as = 'div',
+      className = '',
+      attrs,
+    }: ReactRendererOptions
+  ) {
+    this.id = Math.floor(Math.random() * 0xffffffff).toString();
+    this.component = component;
+    this.editor = editor as ExtendedEditor;
+    this.props = props;
+    this.element = document.createElement(as);
+    this.element.classList.add('react-renderer');
 
     if (className) {
-      this.element.classList.add(...className.split(' '))
+      this.element.classList.add(...className.split(' '));
     }
 
-    this.render()
+    if (attrs) {
+      Object.keys(attrs).forEach((key) => {
+        this.element.setAttribute(key, attrs[key]);
+      });
+    }
+
+    this.render();
   }
 
   render(): void {
-    const Component = this.component
-    const props = this.props
+    const Component = this.component;
+    const props = this.props;
 
     if (isClassComponent(Component) || isForwardRefComponent(Component)) {
       props.ref = (ref: R) => {
-        this.ref = ref
-      }
+        this.ref = ref;
+      };
     }
 
-    this.reactElement = <Component {...props } />
+    this.reactElement = <Component {...props} />;
 
-    this.editor?.contentComponent?.setRenderer(this.id, this)
+    this.editor?.contentComponent?.setRenderer(this.id, this);
   }
 
   updateProps(props: Record<string, any> = {}): void {
     this.props = {
       ...this.props,
       ...props,
-    }
+    };
 
-    this.render()
+    this.render();
   }
 
   destroy(): void {
-    this.editor?.contentComponent?.removeRenderer(this.id)
+    this.editor?.contentComponent?.removeRenderer(this.id);
   }
 }
