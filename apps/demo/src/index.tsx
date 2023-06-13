c057ca96bdba63a230414433f13eaae6d962ce35
HaPBoy <ihapboy@gmail.com>
2023-06-13 11:56:32 +0800

    refactor: remove demo TideEditor component

--- apps/demo/src/index.tsx
+++ apps/demo/src/index.tsx
@@ -1,16 +1,54 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import type { Editor } from '@gitee/tide';
+import { EditorRender, useEditor } from '@gitee/tide';
+import { StarterKit } from '@gitee/tide-starter-kit';
+import { TextAlign } from '@tiptap/extension-text-align';
 import HeaderBar from './components/HeaderBar';
 import InspectPanel from './components/InspectPanel';
-import { TideEditor } from './components/Editor';
+import { MentionMember } from './components/Editor/extensions/mention-member';
+import {
+  mockFetchMemberMentionDebounced,
+  mockImgUploader,
+} from './components/Editor/utils';
 
+import '@gitee/tide/dist/style.css';
+import 'highlight.js/styles/default.css';
 import './index.less';
 
 console.log('BUILD_TIME：', __BUILD_TIME__);
 
 function App() {
-  const [editor, setEditor] = useState<Editor | null>(null);
+  const editor = useEditor({
+    autofocus: true,
+    extensions: [
+      StarterKit.configure({
+        textAlign: false,
+        taskItem: {
+          onReadOnlyChecked: () => true,
+        },
+        uploader: {
+          image: {
+            uploader: mockImgUploader,
+          },
+        },
+      }),
+      TextAlign.extend({
+        addKeyboardShortcuts: () => ({}),
+      }).configure({
+        types: ['heading', 'paragraph'],
+      }),
+      MentionMember.configure({
+        suggestion: {
+          items: ({ query }) => mockFetchMemberMentionDebounced(query),
+        },
+      }),
+    ],
+    readOnlyEmptyView: (
+      <div style={{ padding: 20, textAlign: 'center' }}>
+        <p>暂无内容（只读模式）</p>
+      </div>
+    ),
+  });
 
   return (
     <div className="demo">
@@ -19,7 +57,7 @@ function App() {
       </div>
       <div className="demo-main">
         <div className="demo-editor-wrapper">
-          <TideEditor ref={setEditor} />
+          <EditorRender editor={editor} />
         </div>
         <div className="demo-inspect">
           <InspectPanel editor={editor} />
